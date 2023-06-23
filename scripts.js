5430e41d061b073939d386dba95ea381ee2fd31a
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2023-06-23 02:32:38 -0600

    Add random array generation

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function generateRandomArray(length, min, max) {
+  var arr = [];
+  for (var i = 0; i < length; i++) {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    arr.push(randomNumber);
+  }
+  return arr;
+}
+
+function createBars(arr) {
+  var container = document.getElementById("container");
+  container.innerHTML = "";
+
+  var maxVal = Math.max(...arr);
+  var barHeightScale = 800 / maxVal; // Scale the bar height based on the maximum value
+
+  arr.forEach(function (value) {
+    var bar = document.createElement("div");
+    bar.className = "bar";
+    bar.style.height = value * barHeightScale + "px";
+    container.appendChild(bar);
+  });
+}
+
+document
+  .getElementById("start-sort-button")
+  .addEventListener("click", function () {
+    var arr = generateRandomArray(80, 10, 100);
+    createBars(arr);
+  });
